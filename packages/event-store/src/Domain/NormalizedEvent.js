fbb6f665c8136bc3f74289937372463b24cf69ab
Armand Abric <armand@forgebinaire.net>
2017-12-06 13:17:48 +0100

    qa(namespace): improve directory structure

--- packages/event-store/src/Domain/NormalizedEvent.js
+++ packages/event-store/src/Domain/NormalizedEvent.js
@@ -14,20 +14,25 @@ export default class NormalizedEvent {
   type: string;
   data: PrimitiveEventData;
   metadata: PrimitiveEventData;
+  version: number;
 
   constructor(
     uid: string,
     type: string,
     data: { [key: string]: PrimitiveEventData },
     metadata: { [key: string]: PrimitiveEventData } = {},
+    version: number = 0,
   ) {
     assert(uid);
     assert(type);
     assert(data);
+    assert(metadata);
+    assert(version >= 0);
 
     this.uid = uid;
     this.type = type;
     this.data = data;
     this.metadata = metadata;
+    this.version = version;
   }
 }
